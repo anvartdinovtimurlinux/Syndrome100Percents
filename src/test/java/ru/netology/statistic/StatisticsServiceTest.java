119fe92c907450a87ac18a335a65e46ed3cc713f
Timur Anvartdinov <t.anvartdinov@life-stream.tv>
2021-09-29 11:25:33 +0300

    Добавил плагин JaCoCo. Настроил, чтобы он контролировал BRANCH. То есть тесты заходили во все ветки кода

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,20 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxWhenMaxInBegin() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void findMax2() {
-        StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {11};
-        long expected = 11;
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
